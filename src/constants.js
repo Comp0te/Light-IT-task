c380d6d972bee3ffb791a5500364c4a04dd3fb72
Compote <simbiatoff@gmail.com>
2018-09-29 21:29:20 +0300

    added new constans

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,11 @@ export const LOAD_ALL_PRODUCTS = 'LOAD_ALL_PRODUCTS';
 export const START = 'START';
 export const SUCCESS = 'SUCCESS';
 export const FAIL = 'FAIL';
+export const ACTIVE_PRODUCT = 'ACTIVE_PRODUCT';
+export const LOAD_COMMENTS = 'LOAD_COMMENTS';
+
+export const URL = {
+  LOAD_ALL_PRODUCTS: 'http://smktesting.herokuapp.com/api/products',
+  IMAGE_SRC: 'http://smktesting.herokuapp.com/static/',
+  LOAD_COMMENTS: 'http://smktesting.herokuapp.com/api/reviews/',
+};
