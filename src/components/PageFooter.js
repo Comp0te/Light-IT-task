4538c81e1b029fcb1337224fe27d7ff561712934
Compote <simbiatoff@gmail.com>
2018-09-29 15:11:51 +0300

    added div.page-footer__wraper

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 export default function PageFooter() {
   return (
-    <footer className="page-footer container">
-      <span className="page-footer__author">
-        Created by Dmitry Vinnik
-      </span>
-      <ul className="page-footer__contacts-list">
-        <li className="page-footer__contacts-item">
-          <a className="page-footer__link" href="mailto:simbiatoff@gmail.com">Email address</a>
-        </li>
-        <li className="page-footer__contacts-item">
-          <a className="page-footer__link" href="https://www.linkedin.com/in/vinnik-dmitry" target="_blank" rel="noopener noreferrer">Linkedin</a>
-        </li>
-        <li className="page-footer__contacts-item">
-          <a className="page-footer__link" href="https://github.com/Comp0te" target="_blank" rel="noopener noreferrer">GitHub</a>
-        </li>
-      </ul>
+    <footer className="page-footer">
+      <div className="container">
+        <span className="page-footer__author">
+          Created by Dmitry Vinnik
+        </span>
+        <ul className="page-footer__contacts-list">
+          <li className="page-footer__contacts-item">
+            <a className="page-footer__link" href="mailto:simbiatoff@gmail.com">Email address</a>
+          </li>
+          <li className="page-footer__contacts-item">
+            <a className="page-footer__link" href="https://www.linkedin.com/in/vinnik-dmitry" target="_blank" rel="noopener noreferrer">Linkedin</a>
+          </li>
+          <li className="page-footer__contacts-item">
+            <a className="page-footer__link" href="https://github.com/Comp0te" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
