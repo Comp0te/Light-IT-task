c1a645cae3a557e260e30f87302ae71f1e9288fc
Compote <simbiatoff@gmail.com>
2018-09-29 15:12:46 +0300

    added div.page-header__wraper and inport new component

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,20 +1,18 @@
 import React from 'react';
+import User from './User';
 
 export default function PageHeader() {
   return (
-    <header className="page-header container">
-      <p className="page-header__title">
-        Test task for the company
-        <a className="page-header__light-it" href="https://light-it.net" target="_blank" rel="noopener noreferrer">
-          Light IT
-        </a>
-      </p>
-      <button className="page_header__sing-in button" type="button">
-        Sing in
-      </button>
-      <button className="page_header__register button" type="button">
-        Registration
-      </button>
+    <header className="page-header">
+      <div className="page-header__wraper container">
+        <p className="page-header__title">
+          {'Test task for the company: '}
+          <a className="page-header__light-it" href="https://light-it.net" target="_blank" rel="noopener noreferrer">
+            Light IT
+          </a>
+        </p>
+        <User />
+      </div>
     </header>
   );
 }
