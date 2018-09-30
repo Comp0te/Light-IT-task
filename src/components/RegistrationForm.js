586c96481b999dfccc645e2f69e2e3ebcbc337de
Compote <simbiatoff@gmail.com>
2018-09-30 22:56:11 +0300

    added submiting form

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 
 function RegistrationForm(props) {
-  const { isVisible, closeUp } = props;
+  const {
+    isVisible, closeUp, handleSubmit, isLoading,
+  } = props;
 
   return (
     <div
@@ -11,7 +13,7 @@ function RegistrationForm(props) {
         ? 'registration-modal registration-modal--on modal'
         : 'registration-modal modal'}
     >
-      <form className="registration-modal__form">
+      <form onSubmit={handleSubmit} className="registration-modal__form">
         <h2 className="registration-modal__heading">
           Registration form
         </h2>
@@ -24,22 +26,23 @@ function RegistrationForm(props) {
           <Field className="text-input" name="lastName" component="input" type="text" id="reg-last-name" />
         </div>
         <div className="registration-modal__field">
-          <label className="label" htmlFor="reg-user-name">User name: </label>
-          <Field className="text-input" name="userName" component="input" type="text" id="reg-user-name" />
+          <label className="label" htmlFor="reg-user-name">User name: * </label>
+          <Field className="text-input" name="username" component="input" type="text" id="reg-user-name" required />
         </div>
         <div className="registration-modal__field">
           <label className="label" htmlFor="reg-user-email">Email: </label>
-          <Field className="text-input" name="email" component="input" type="email" id="reg-user-email" />
+          <Field className="text-input" name="email" component="input" type="email" id="reg-user-email" placeholder="example@examp.com" />
         </div>
         <div className="registration-modal__field registration-modal__field--password">
-          <label className="label label--password" htmlFor="reg-user-password">User password: </label>
-          <Field className="text-input text-input--password" name="userPassword" component="input" type="password" id="reg-user-password" />
-          <Field className="text-input text-input--password" name="userPassword" component="input" type="password" />
+          <label className="label label--password" htmlFor="reg-user-password">User password: * </label>
+          <Field className="text-input text-input--password" name="password" component="input" type="password" id="reg-user-password" placeholder="Ented the password" required />
         </div>
+        <p className="registration-modal__required">Fields marked with an asterisk * are required!</p>
         <div className="registration-modal__buttons-wraper">
           <button
             className="registration-modal__button button"
             type="submit"
+            disabled={isLoading}
           >
           Register
           </button>
@@ -58,7 +61,10 @@ function RegistrationForm(props) {
 
 RegistrationForm.propTypes = {
   isVisible: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   closeUp: PropTypes.func.isRequired,
+  // from redux-form
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default reduxForm({
