bde45a8979e628052cc5752afca055819cbba86d
Compote <simbiatoff@gmail.com>
2018-10-02 01:26:57 +0300

    added new component - custom form field

---
+++ src/components/FormField.js
@@ -0,0 +1,43 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+export default function FormField(props) {
+  const {
+    id,
+    className,
+    placeholder,
+    input,
+    labelText,
+    type,
+    meta: { touched, error },
+  } = props;
+
+  return (
+    <div className={className.wraper}>
+      <label className={className.label} htmlFor={id}>{labelText}</label>
+      {type !== null
+        ? (
+          <input
+            {...input}
+            type={type}
+            className={className.input}
+            id={id}
+            placeholder={placeholder}
+          />
+        )
+        : (
+          <textarea
+            {...input}
+            className={className.input}
+            id={id}
+            placeholder={placeholder}
+          />
+        )}
+      {touched && (error && <span className={className.error}>{error}</span>)}
+    </div>
+  );
+}
+
+FormField.propTypes = {
+
+};
